2fd9d688589c2bf2fa381361a52a938bf0e575cd
ale110i <ishhenko.as@phystech.edu>
2019-10-03 10:43:49 +0300

    Initial Commit

---
+++ RMQ_2nd_stats/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  RMQ_2nd_stats
+//
+//  Created by Alexey Ischenko on 03.10.2019.
+//  Copyright © 2019 Enthropy. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
